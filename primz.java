ebc4a0a8e3d1dd19fc000e5ffaed887e4ff6c552
attikoszto <131347235+attikoszto@users.noreply.github.com>
2023-11-23 15:50:59 +0100

    Update primz.java

--- primz.java
+++ primz.java
@@ -3,26 +3,18 @@ package Attila;
 import java.awt.desktop.SystemEventListener;
 
 public class primz {
-    ;
-
+    
     public static void main(String[] args) {
         String Primenumbers = "";
         int lastprime= 0;
         int totalCount = 0;
         String lastnumber= "";
 
-
-        
-
-
         for (int i = 0; i < 1000; i++) {
             int counter = 0;
-
-
             for (int num = i; num >= 1; num--) {
                 if (i % num == 0) {
                     counter +=  1;
-
                 }
             }
             if ( totalCount == 50)
@@ -33,16 +25,8 @@ public class primz {
                 totalCount++;
             if(totalCount == 50){
                 lastprime =  i;
-
-
             }
-
-
-
-
-
             }
-
         }
         System.out.println(Primenumbers);
         System.out.println("anzahl der Primzahlen sind : " +totalCount);
